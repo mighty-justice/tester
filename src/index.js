61d00a4aabdac4c9dd5f820c5859068b3fefbe54
Keven Bouchard <peckgfx@gmail.com>
2018-12-20 11:26:59 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Tester from './tester';
-import ConfigurationClass from './ConfigurationClass';
-
-const TesterConfig = new ConfigurationClass(Tester);
-
-export {
-  Tester,
-  TesterConfig,
-}
