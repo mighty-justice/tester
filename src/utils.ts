afdff225176cf5f11b728336e9f5209a1e3ce790
Robert Kuykendall <robert@rkuykendall.com>
2019-02-15 14:44:54 -0500

    Add changeInput, click, and submit utilities (#16)

--- src/utils.ts
+++ src/utils.ts
@@ -2,26 +2,23 @@
   Utilities
 */
 
-function getInstance (component: any) {
+export function getInstance (component: any) {
   const instance = component.instance();
   return instance.wrappedInstance || instance;
 }
 
-function getValue (tester: any, value: unknown) {
+export function getValue (tester: any, value: unknown) {
   return typeof value === 'function' ? value(tester) : value;
 }
 
-async function sleep (ms: number = 0) {
+export async function sleep (ms: number = 0) {
   return new Promise<void>(resolve => setTimeout(resolve, ms));
 }
 
-function capitalize (string: string) {
+export function capitalize (string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export {
-  capitalize,
-  getInstance,
-  getValue,
-  sleep,
-};
+export function isString (value: unknown): value is string {
+  return typeof value === 'string' || value instanceof String;
+}
