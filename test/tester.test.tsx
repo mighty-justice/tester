e90de594a521419985ddf1b868ae9d0fc952ceaf
SHA-4 <alexm@mighty.com>
2020-03-05 17:18:18 -0500

    Add test for async componentDidMount

--- test/tester.test.tsx
+++ test/tester.test.tsx
@@ -1,13 +1,35 @@
 /* global it, describe, expect */
-import React from 'react';
+import React, { Component } from 'react';
 import { Tester } from '../src';
+import { sleep } from '../src/utils';
 
 const COMPONENT_ID = 'testing-component';
 
 const MyTestingComponent = (props: any) => <div id={COMPONENT_ID} {...props} />;
 
-describe('Tester', () => {
+class AsyncComponent extends Component {
+  public constructor (props: object) {
+    super(props);
+    this.state = { status: 'loading' };
+  }
 
+  public async componentDidMount () {
+    await sleep(10);
+    this.setState({
+      status: 'done',
+    });
+  }
+
+  public render () {
+    return (
+      <div>
+        {this.state.status}
+      </div>
+    );
+  }
+}
+
+describe('Tester', () => {
   it('Init tests', async () => {
     const tester = await new Tester(MyTestingComponent).mount();
     expect(tester.wrapper).toBeTruthy();
@@ -19,4 +41,10 @@ describe('Tester', () => {
     expect(tester.wrapper).toBeTruthy();
     expect(tester.wrapper.html()).toContain('test-hook-component');
   });
+
+  it('Awaits async componentDidMount', async () => {
+    const tester = await new Tester(AsyncComponent).mount();
+    expect(tester.wrapper.text()).toContain('done');
+  });
+
 });
